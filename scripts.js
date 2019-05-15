23e90f8a3b16f1c47134a2f80e593d859a627e57
Michele Conway <mconway890@gmail.com>
2019-05-15 12:35:15 -0400

    Add variables to manage flip state, change toggle method to add

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
