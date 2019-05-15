51266a1384115bd0eeaf3c87c40c0dbe0f175300
Michele Conway <mconway890@gmail.com>
2019-05-15 13:06:16 -0400

    Add logic to handle matching

--- scripts.js
+++ scripts.js
@@ -9,7 +9,38 @@ function flipCard() {
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    return;
   }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  // if (firstCard.dataset.framework === secondCard.dataset.framework) {
+  //   disableCards();
+  //   return;
+  // }
+  //
+  // unflipCards();
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+  isMatch ? disableCards() : unflipCards();
+}
+
+// prevent further flipping in case of a match
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+// turn cards back, removes flip class
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
